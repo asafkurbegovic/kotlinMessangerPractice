d0dc3779fddaccba4488acd497ea62b910aab0b9
asafkurbegovic <asafkurbegovic@gmail.com>
2020-12-15 23:53:18 +0100

    Hurry up, some layouts added

--- app/src/main/java/com/asaf/kotlinmessanger/NewMessageActivity.kt
+++ app/src/main/java/com/asaf/kotlinmessanger/NewMessageActivity.kt
@@ -2,11 +2,29 @@ package com.asaf.kotlinmessanger
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
         supportActionBar?.title="Select User"
+
+        val adapter = GroupAdapter<GroupieViewHolder>()
+        recyclerView_newMessage.adapter = adapter
     }
+}
+
+class UserItem : Item<GroupieViewHolder>() {
+    override fun bind(viewHolder: GroupieViewHolder, position: Int) {
+
+    }
+
+    override fun getLayout(): Int {
+         R.layout.user_row_layout
+    }
+
 }
